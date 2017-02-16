ef97157a52a52c1bd8a2edbfe4087cf663535e38
Nicholas Ouellette <nicho@nb.sympatico.ca>
2017-02-15 21:16:27 -0400

    Add files via upload

---
+++ Pokemon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pokedex;
+
+/**
+ *
+ * @author Nicholas
+ */
+public class Pokemon {
+
+    int Number;
+    String Name, Classifaction, Pokedex, Gender, Hight, Mass;
+
+    public Pokemon(int NumberOfPokemon, String PokemonName, String Class) {
+
+        Number = NumberOfPokemon;
+        Name = PokemonName;
+        Classifaction = Class;
+
+    }
+
+}
